f3a30541b11092e5b3b50189c56095399f0302cd
Loren Wetzel <l.wetzel900@gmail.com>
2019-10-05 08:23:12 -0500

    finishing up ems app

--- ems/app.js
+++ ems/app.js
@@ -20,11 +20,13 @@ var cookieParser = require("cookie-parser");
 var csrf = require("csurf");
 var Employee = require("./models/employee");
 
+
 //mLab connection
 var mongoDB = "mongodb+srv://admin:admin@buwebdev-cluster-1-7jtao.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, {
   useMongoClient: true
 });
+
 mongoDB.Promise = global.Promise;
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "\nMongoDB connection error: "));
@@ -32,14 +34,18 @@ db.once("open", function () {
   console.log("\nApplication connected to mLab MongoDB instance\n");
 });
 
+
 //set up CSRF protection
-let csrfProtection = csrf({ cookie: true });
+var csrfProtection = csrf({ cookie: true });
+
 
 //application
 var app = express();
 
+
 //morgan library
 app.use(logger("short"));
+
 
 //body parser
 app.use(
@@ -48,14 +54,18 @@ app.use(
   })
 );
 
+
 //cookie parser
 app.use(cookieParser());
+
 
 //using helmet to help prevent XSS attacks
 app.use(helmet.xssFilter());
 
+
 //CSRF protection
 app.use(csrfProtection);
+
 
 //intercepts all incoming requests and adds a CSRF token to the response
 app.use(function (req, res, next) {
@@ -65,34 +75,32 @@ app.use(function (req, res, next) {
   next();
 });
 
-//model
-var employee = new Employee({
-  firstName: "Loren",
-  lastName: "Wetzel"
-});
 
 //view engine, view directory path and server port
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 3030);
 
+
 //routing
+//home
 app.get("/", function (req, res) {
   res.render("index", {
     title: "E.M.S.",
   });
 });
 
+
+//creates new employee
 app.get("/new", function (req, res) {
   res.render("new", {
     title: "New Entry"
   });
 });
 
+
 //process forms
 app.post("/process", function (req, res) {
-  // console.log("Name is: " + req.body.fName);
-  // res.redirect("/");
   if (!req.body.txtFirstName && !req.body.txtLastName) {
     res.status(400).send("Entries must have a first and last name");
     return;
@@ -103,20 +111,22 @@ app.post("/process", function (req, res) {
   var lName = req.body.txtLastName;
   console.log("\n" + fName + " " + lName);
 
-  //create employee model
-  var employee = new Employee({
-    firstName: fName,
-    lastName: lName
-  });
+  //model
+var employee = new Employee({
+  firstName: fName,
+  lastName: lName
+});
 
   //save
   employee.save(function (err) {
     if (err) throw err;
     console.log("\n" + fName + " " + lName + " saved successfully!!!!");
   });
-  res.redirect("/");
+  res.redirect("/list");
 });
 
+
+//shows list of employees
 app.get("/list", function (req, res) {
 
   Employee.find({}, function (error, employees) {
@@ -129,11 +139,25 @@ app.get("/list", function (req, res) {
   });
 });
 
-app.get("/view", function (req, res) {
-  res.render("view", {
-    title: "Individual Employee Information"
+
+//get individual employee
+app.get("/view/:queryName", function (req, res) {
+  var queryName = req.params.queryName;
+
+  Employee.find({ 'firstName': queryName }, function (error, employees) {
+    if (error) throw error;
+
+    if (employees.length > 0) {
+      res.render("view", {
+        title: "Individual Employee Information",
+        employee: employees
+      });
+    } else {
+      res.redirect("/list");
+    }
   });
 });
+
 
 http.createServer(app).listen(app.get("port"), function () {
   console.log("\nApp started. localhost:" + app.get("port") + " copy and paste to see it !!!\n");
